5ba39207932d237556d0995243bcd102aff6565f
a1usha <aleksandr.ushaev@hotmail.com>
2021-04-11 12:08:00 +0700

    delete garb

--- build/resources/test/empty_strings.py
+++
@@ -1,3 +0,0 @@
-empty_string_1 = ""
-empty_string_2 = ""
-empty_string_3 = ""
--- build/resources/test/test.py
+++
@@ -1,7 +0,0 @@
-database = [{'id': i, "value": str(i)} for i in range(100)]
-
-print("Hello world!")
-
-for data in database:
-    if data['id'] % 10 == 0 or data['id'] == 13:
-        print(data['value'])
